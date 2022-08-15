e901ecdf99dcb851298cbec72271c9d0b6c9b519
Sahil Kavitake <sahil.kavitake@bluepineapple.io>
2022-08-15 23:58:08 +0530

    removed unused useEffect import

--- src/components/Card/PersonCard.js
+++ src/components/Card/PersonCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import CardIconText from "./CardIconText";
 import { MailOutlined, PhoneOutlined, GlobalOutlined } from "@ant-design/icons";
 import CardActions from "./CardActions";
